025f2903c53c8d88d969d1de315141d9365d73d0
mmiyaji <ruhenheim1223@gmail.com>
2012-06-07 00:02:05 +0900

    add gitignore

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mmiyaji
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hoge");
+	}
+
+}
